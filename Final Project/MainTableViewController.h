60fb1cd24fb33250a13536f152bc2de2b2b48041
jmgabrielson <Atropos@MDACT.local>
2014-11-15 11:56:08 -0600

    Initial Setup
    Created and associated files with view controllers. Set up basic UI and
    segues.

--- Final Project/MainTableViewController.h
+++ Final Project/MainTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainTableViewController.h
 //  Final Project
 //
 //  Created by James Gabrielson on 11/15/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface MainTableViewController : UITableViewController
 
 @end
---
+++ Final Project/NewActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewActivityViewController.h
+//  Final Project
+//
+//  Created by James Gabrielson on 11/15/14.
+//  Copyright (c) 2014 James Gabrielson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewActivityViewController : UIViewController
+
+@end
