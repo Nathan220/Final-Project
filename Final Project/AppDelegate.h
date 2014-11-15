cb9036db9e430819d92c95b3e0b411d07d3a1f37
James Gabrielson <Atropos@MDACT.local>
2014-11-15 11:32:01 -0600

    Initial Commit

---
+++ Final Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Final Project
+//
+//  Created by James Gabrielson on 11/15/14.
+//  Copyright (c) 2014 James Gabrielson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Final Project/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Final Project
+//
+//  Created by James Gabrielson on 11/15/14.
+//  Copyright (c) 2014 James Gabrielson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
